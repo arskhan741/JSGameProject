eaaaf7e444e8d1ddc5b5d4dbe221e04ad0fa878d
Waleed Shahid <waleed.shahid@logicps.com>
2024-10-23 16:19:26 +0500

    Added suggestions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { GameOutCome } from "./game.js";
 
+//console.log(`cookie = ${document.cookie}`);
+
 const gameOutCome = new GameOutCome();
 const box = document.getElementById("box");
 
@@ -7,9 +9,8 @@ const box = document.getElementById("box");
 document.getElementById("gift-box").classList.add("hidden");
 document.getElementById("continueSection").classList.add("hidden");
 
-async function getData(token) {
-  const url =
-    "https://mp-staging.co.uk/master/api/rewards/api/rewards/abc123/613/14486/22aa99d8c8aba6fd3866766f33b6d8146e3892ec";
+async function getData(token, requestData) {
+  const url = `https://mp-staging.co.uk/master/api/rewards/api/rewards/${requestData.userIdentifier}/${requestData.promoId}/${requestData.fixtureId}/22aa99d8c8aba6fd3866766f33b6d8146e3892ec`;
 
   try {
     const response = await fetch(url, {
@@ -47,9 +48,17 @@ const handleClick = async () => {
 
   box.classList.toggle("open");
 
-  const response = await getData(token);
+  // Send the required API Parameters. I am testing it with default values
+  const requestData = {
+    userIdentifier: "abc123",
+    promoId: 613,
+    fixtureId: 14486,
+  };
 
-  console.log(response);
+  const response = await getData(token, requestData);
+
+  // Add a 1 second delay
+  await new Promise((resolve) => setTimeout(resolve, 500));
 
   gameOutCome.ShowResultText(response);
 };
